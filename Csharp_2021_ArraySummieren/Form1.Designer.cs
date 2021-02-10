f985c94c3282c445f37621d95ed73db0f0e37923
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-10 17:00:24 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_ArraySummieren/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Csharp_2021_ArraySummieren
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnErgebnis = new System.Windows.Forms.Button();
+			this.lblErgebnis = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnErgebnis
+			// 
+			this.btnErgebnis.Location = new System.Drawing.Point(60, 57);
+			this.btnErgebnis.Name = "btnErgebnis";
+			this.btnErgebnis.Size = new System.Drawing.Size(75, 23);
+			this.btnErgebnis.TabIndex = 0;
+			this.btnErgebnis.Text = "Ergebnis";
+			this.btnErgebnis.UseVisualStyleBackColor = true;
+			this.btnErgebnis.Click += new System.EventHandler(this.btnErgebnis_Click);
+			// 
+			// lblErgebnis
+			// 
+			this.lblErgebnis.AutoSize = true;
+			this.lblErgebnis.Location = new System.Drawing.Point(22, 13);
+			this.lblErgebnis.Name = "lblErgebnis";
+			this.lblErgebnis.Size = new System.Drawing.Size(35, 13);
+			this.lblErgebnis.TabIndex = 1;
+			this.lblErgebnis.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(217, 98);
+			this.Controls.Add(this.lblErgebnis);
+			this.Controls.Add(this.btnErgebnis);
+			this.Name = "Form1";
+			this.Text = "Array summieren";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnErgebnis;
+		private System.Windows.Forms.Label lblErgebnis;
+	}
+}
+
---
+++ Csharp_2021_ArraySummieren/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_ArraySummieren
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnErgebnis_Click(object sender, EventArgs e)
+		{
+			int[] zahlenreihe = { 7, 13, 25, 16, 8, 2 };
+
+			// Anzeige wird geleert
+			lblErgebnis.Text = "";
+
+			// Tipp aus der Praxis: Jede Funktion, jede Schleife hat genau eine Aufgabe
+
+			// Rechenweg ausgeben
+			for (int i = 0; i < zahlenreihe.Length - 1; i++)
+			{
+				lblErgebnis.Text += zahlenreihe[i] + " + ";
+			}
+
+			// Letzte Zahl & Ergebnis anhängen und ausgeben
+			lblErgebnis.Text += 
+				zahlenreihe[zahlenreihe.Length - 1] + 
+				" = " + 
+				ArraysummeBilden(zahlenreihe);
+		}
+
+		// Funktion addiert zwei Werte ( a und b)
+		// gibt das Ergebnis zurück
+		private int Addieren(int a, int b)
+		{
+			int summe = a + b;
+			return summe;
+
+			// Kurzform: 
+			// return a + b;
+		}
+
+		private int ArraysummeBilden(int[] x)
+		{
+			int summe = 0;
+
+			for (int i = 0; i < x.Length; i++)
+			{
+				summe += x[i];
+			}
+
+			return summe;
+		}
+
+
+	}
+}
